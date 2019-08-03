c4188ab7b4bbbc1ceaf96cc794f86ff07d1dd55f
dsstewa <davidstewart@gmail.com>
2019-08-03 16:39:05 -0600

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,8 +3,26 @@ require 'pry'
 class SchoolClassesController < ApplicationController
      
     def create
-        binding.pry
+        @school = SchoolClass.new(post_params)
+       
+        @school.save
+        
+        redirect_to school_class_path(@school)
     end
+
+  def new
+     @school_class = SchoolClass.new
+  end
+
+
+
+    private
+ 
+    def post_params
+      params.require(:school_class).permit(:room_number, :title)   #This privat lass is defining the :school_class and permiting only the room number and title 
+    
+    end
+
 
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,26 @@
 class StudentsController < ApplicationController
     
+   def create
+    @student = Student.new(post_params)
+    @student.save
+    redirect_to student_path(@student)
+   end
+
+
+
+   def new
+      binding.pry
+   end
+
+   private
+ 
+   def post_params
+     params.require(:student).permit(:first_name, :last_name)   #This privat lass is defining the :school_class and permiting only the room number and title 
+   
+   end
+
+
+
+
+
 end
